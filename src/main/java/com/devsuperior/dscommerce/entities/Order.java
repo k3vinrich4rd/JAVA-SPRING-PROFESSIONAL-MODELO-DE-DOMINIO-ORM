1f9bc9f02653cab3c705788f6d0b29287de8c3ae
k3vinrich4rd <kevinrichardve@gmail.com>
2025-10-20 15:09:39 -0300

    feat: Implementando OrderItem e OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -4,6 +4,9 @@ import com.devsuperior.dscommerce.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,6 +29,9 @@ public class Order {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     @JoinColumn(name = "payment_id")
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrdemItem> items = new HashSet<>();
 
     public Order() {
 
@@ -77,5 +83,16 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+
+    public Set<OrdemItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
+
+
     }
 }
