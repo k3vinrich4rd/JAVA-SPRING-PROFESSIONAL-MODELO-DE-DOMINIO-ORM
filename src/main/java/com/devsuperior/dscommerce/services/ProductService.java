89c0a053579bd805d043aa7dac44ab260db7c328
k3vinrich4rd <kevinrichardve@gmail.com>
2026-09-24 09:10:22 -0300

    feat: implementando delete para product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -60,8 +60,10 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
     }
 
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
 
 }
